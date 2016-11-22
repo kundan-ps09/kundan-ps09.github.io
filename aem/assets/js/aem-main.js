edb12ecf3514b8e19c346f3c7bbb41d8f975f047
Kundan Singh <ksing8@sapient.com>
2016-11-22 11:33:21 +0530

    comit msg

--- aem/assets/js/aem-main.js
+++ aem/assets/js/aem-main.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
  $("#head-nav").load("header.html"); 	
  $("#left-nav").load("left-navigation.html"); 
  $("#footer-nav").load("footer.html"); 
- $("right-nav").load("right-navigation.html");
+ $("#right-nav").load("right-navigation.html");
  
 
 });
