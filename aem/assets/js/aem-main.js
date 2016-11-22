6ad333c95b9c7091ddc1ee3f5a8aa89675471b1e
Kundan Singh <ksing8@sapient.com>
2016-11-22 11:25:15 +0530

    comit msg

--- aem/assets/js/aem-main.js
+++ aem/assets/js/aem-main.js
@@ -2,7 +2,6 @@ $(document).ready(function(){
  $("#head-nav").load("header.html"); 	
  $("#left-nav").load("left-navigation.html"); 
  $("#footer-nav").load("footer.html"); 
-// $("#display-date").append('&copy;'+ new Date().getFullYear());
-// var currentYear = new Date().getFullYear();
-// $("#display-date").Html("hello"+currentYear);
+ $("right-nav").load("right-navigation.html");
+
 });
